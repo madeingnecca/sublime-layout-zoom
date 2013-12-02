1f22deabe8d0389bad723abe01d40c8bed1703d5
Damiano Seno <dax86it@gmail.com>
2013-12-02 21:19:21 +0100

    better implementation + better docs

--- layout-zoom.py
+++ layout-zoom.py
@@ -1,42 +1,119 @@
 import sublime
 import sublime_plugin
+import json
+import os
 
-class LayoutZoomCommand(sublime_plugin.TextCommand):
+STORAGE = sublime.packages_path() + '/User/LayoutZoomStorage.json'
 
-    def run(self, edit, op='get'):
-        window = self.view.window()
-        settings = sublime.load_settings('LayoutZoomStore')
+class BaseLayoutCommand(sublime_plugin.WindowCommand):
+    def defaultData(self):
+        return {
+            "windows": {}
+        }
 
-        if op == 'get':
-            layout = window.get_layout()
+    def storageRead(self):
+        data = {}
 
-            groups = []
-            for group_index in range(0, len(layout['cells'])):
-                views = window.views_in_group(group_index)
-                groups.append(len(views))
+        if os.path.exists(STORAGE):
+            with open(STORAGE) as f:
+                data = json.loads(f.read() or '{}')
 
-            # Set single layout, the "zoom" effect.
-            window.set_layout({"cols": [0.0, 1.0], "rows": [0.0, 1.0], "cells": [[0, 0, 1, 1]]})
+        # Merge defaultData and data.
+        return dict(self.defaultData(), **data)
 
-            # Save state on disk
-            settings.set('groups', groups)
-            settings.set('layout', layout)
-            sublime.save_settings('LayoutZoomStore')
+    def storageWrite(self,data):
+        content = json.dumps(data, sort_keys=False, indent=4, separators=(',', ': '))
+        with open(STORAGE, 'w') as f:
+            f.write(content)
 
-        elif op == 'restore':
-            layout = settings.get('layout')
-            groups = settings.get('groups')
+class LayoutZoomCommand(BaseLayoutCommand):
 
-            # Restore layout
-            window.set_layout(layout)
+    def zoom(self, window):
+        # Set single layout, the "zoom" effect.
+        window.set_layout({
+            'cols': [0.0, 1.0],
+            'rows': [0.0, 1.0],
+            'cells': [[0, 0, 1, 1]]})
 
-            views = window.views()
+    def run(self):
+        window = self.window
+        wid = str(window.id())
 
-            # Restore views in layout
-            view_it = 0
-            for group_index in range(0, len(groups)):
-                group = groups[group_index]
-                for view_index in range(0, group):
-                    view = views[view_it]
-                    window.set_view_index(view, group_index, view_index)
-                    view_it += 1
+        data = self.storageRead()
+
+        layout = window.get_layout()
+
+        # Remember the count of views per each group.
+        groups = []
+        for group_index in range(0, len(layout['cells'])):
+            views = window.views_in_group(group_index)
+            group = {}
+            group['count'] = len(views)
+            active_view = window.active_view_in_group(group_index)
+            active_view_index = window.get_view_index(active_view)
+            group['active_view'] = active_view_index[1]
+            groups.append(group)
+
+        active_group = window.active_group()
+
+        self.zoom(window)
+
+        data['windows'][wid] = {}
+        data['windows'][wid]['layout'] = layout
+        data['windows'][wid]['groups'] = groups
+        data['windows'][wid]['active_group'] = active_group
+
+        self.storageWrite(data)
+
+
+class LayoutZoomRestoreCommand(BaseLayoutCommand):
+    def run(self):
+        window = self.window
+        wid = str(window.id())
+
+        data = self.storageRead()
+
+        if not wid in data['windows']:
+            return
+
+        layout = data['windows'][wid]['layout']
+        groups = data['windows'][wid]['groups']
+        active_group = data['windows'][wid]['active_group']
+
+        views = window.views()
+
+        # Restore layout
+        window.set_layout(layout)
+
+        # Restore views in layout
+        view_index = 0
+        for group_index in range(len(groups)):
+            group = groups[group_index]
+            group_active_view = None
+            for view_in_group in range(group['count']):
+                view = views[view_index]
+                window.set_view_index(view, group_index, view_in_group)
+                view_index += 1
+                if view_in_group == group['active_view']:
+                    group_active_view = view
+            # Give focus to group's active group.
+            window.focus_view(group_active_view)
+        # Give focus to the active group.
+        window.focus_group(active_group)
+
+
+class LayoutZoomCleanCommand(BaseLayoutCommand):
+    def run(self):
+        ids = [str(w.id()) for w in sublime.windows()]
+        data = self.storageRead()
+        wins = data['windows'].keys()
+
+        # Window ids referencing to non-existing windows.
+        diff = list(set(wins) - set(ids))
+
+        # Remove old ids from the storage
+        for winid in diff:
+            data['windows'].pop(winid, None)
+
+        # Save the storage.
+        self.storageWrite(data)
